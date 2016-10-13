7bdac389d0050beba76f600d65d3f56d30d92d2a
Rob Phoenix <robertphoenix@gmail.com>
2016-10-13 16:30:28 +0100

    exercise 1.3 add heading

--- ch01/fahr_to_cels_convert.c
+++ ch01/fahr_to_cels_convert.c
@@ -8,14 +8,18 @@ int main()
 	float fahr, celsius;
 	int lower, upper, step;
 
-	lower = 10;		/* lower limit of temperature table */
+	lower = 0;		/* lower limit of temperature table */
 	upper = 300;		/* upper limit */
 	step = 20;		/* step size */
 
 	fahr = lower;
+	printf("\n+------------+---------+\n");
+	printf("| Fahrenheit | Celsius |\n");
+	printf("+------------+---------+\n");
 	while (fahr <= upper) {
 		celsius = (5.0 / 9.0) * (fahr - 32.0);
-		printf("%3.0f %6.1f\n", fahr, celsius);
+		printf("| %9.0f  | %6.1f  |\n", fahr, celsius);
 		fahr = fahr + step;
 	}
+	printf("+------------+---------+\n\n");
 }
