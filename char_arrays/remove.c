331dd13c760395ccb256f9df767cb0868bc724cc
Rob Phoenix <robphoenix@protonmail.com>
2016-11-04 17:05:36 +0000

    add remove.c

---
+++ char_arrays/remove.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#define MAXLINE 1000		/* maximum input line size */
+
+int fetchline(char line[], int maxline);
+int remove(char s[]);
+
+/* remove trailing blanks and tabs, and delete blank lines */
+int main()
+{
+	char line[MAXLINE];	/* current input line */
+
+	while (fetchline(line, MAXLINE) > 0) {
+		if (remove(line) > 0) {
+			printf("\n", line);
+		}
+	}
+	return 0;
+}
+
+/* remove trailing blanks and tans from character string s */
+int remove(char s[])
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\n') {	/* find newline character */
+		++i;
+	}
+	--i;			/* back off from '\n' */
+	while (i > 0 && (s[i] == ' ' || s[i] == '\t')) {
+		--i;
+	}
+	if (i >= 0) {		/* is it a nonblank line? */
+		++i;
+		s[i] = '\n';	/* put newline character back */
+		++i;
+		s[i] = '\0';	/* terminate the string */
+	}
+	return i;
+}
+
+/* fetchline: read a line into s, return length */
+int fetchline(char s[], int lim)
+{
+	int c, i;
+
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+		s[i] = c;
+	}
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
