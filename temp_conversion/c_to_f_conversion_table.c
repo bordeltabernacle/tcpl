bd38755e5b45adae93f357a52214436a8c52a92b
Rob Phoenix <rob@robphoenix.com>
2017-06-15 20:37:49 +0100

    format temp_conversion files

--- temp_conversion/c_to_f_conversion_table.c
+++ temp_conversion/c_to_f_conversion_table.c
@@ -3,23 +3,22 @@
 /* print Celsius-Fahrenheit table
    for celsius = 0, 20, ..., 300 */
 
-int main()
-{
-	float celsius, fahr;
-	int lower, upper, step;
+int main() {
+  float celsius, fahr;
+  int lower, upper, step;
 
-	lower = 0;		/* lower limit of temperature table */
-	upper = 300;		/* upper limit */
-	step = 20;		/* step size */
+  lower = 0;   /* lower limit of temperature table */
+  upper = 300; /* upper limit */
+  step = 20;   /* step size */
 
-	celsius = lower;
-	printf("\n+---------+------------+\n");
-	printf("| Celsius | Fahrenheit |\n");
-	printf("+---------+------------+\n");
-	while (celsius <= upper) {
-		fahr = ((celsius * 9.0) / 5) + 32;
-		printf("| %6.0f  | %9.1f  |\n", celsius, fahr);
-		celsius = celsius + step;
-	}
-	printf("+---------+------------+\n\n");
+  celsius = lower;
+  printf("\n+---------+------------+\n");
+  printf("| Celsius | Fahrenheit |\n");
+  printf("+---------+------------+\n");
+  while (celsius <= upper) {
+    fahr = ((celsius * 9.0) / 5) + 32;
+    printf("| %6.0f  | %9.1f  |\n", celsius, fahr);
+    celsius = celsius + step;
+  }
+  printf("+---------+------------+\n\n");
 }
--- temp_conversion/f_to_c_conversion_table.c
+++ temp_conversion/f_to_c_conversion_table.c
@@ -3,23 +3,22 @@
 /* print Fahrenheit-Celsius table
    for fahr = 0, 20, ..., 300 */
 
-int main()
-{
-	float fahr, celsius;
-	int lower, upper, step;
+int main() {
+  float fahr, celsius;
+  int lower, upper, step;
 
-	lower = 0;		/* lower limit of temperature table */
-	upper = 300;		/* upper limit */
-	step = 20;		/* step size */
+  lower = 0;   /* lower limit of temperature table */
+  upper = 300; /* upper limit */
+  step = 20;   /* step size */
 
-	fahr = lower;
-	printf("\n+------------+---------+\n");
-	printf("| Fahrenheit | Celsius |\n");
-	printf("+------------+---------+\n");
-	while (fahr <= upper) {
-		celsius = (5.0 / 9.0) * (fahr - 32.0);
-		printf("| %9.0f  | %6.1f  |\n", fahr, celsius);
-		fahr = fahr + step;
-	}
-	printf("+------------+---------+\n\n");
+  fahr = lower;
+  printf("\n+------------+---------+\n");
+  printf("| Fahrenheit | Celsius |\n");
+  printf("+------------+---------+\n");
+  while (fahr <= upper) {
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("| %9.0f  | %6.1f  |\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+  printf("+------------+---------+\n\n");
 }
